ef67e0b4d5c97b17ba1536b9714d30a061326098
edwinacubillos <edwinacubillos@gmail.com>
2024-04-15 14:17:11 -0500

    Paso 2 (18-abril) Se carga la lista de libros de Firebase por uid del usuario, se crea un card para la visualización del libro

--- lib/pages/my_books_page.dart
+++ lib/pages/my_books_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'new_book_page.dart';
@@ -19,12 +21,68 @@ class _MyBooksPageState extends State<MyBooksPage> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: const Center(
-        child: Text('Mis Libros'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("users")
+              .doc(FirebaseAuth.instance.currentUser?.uid)
+              .collection("books")
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+            return ListView.builder(
+              itemCount: snapshot.data?.docs.length,
+              itemBuilder: (context, index) {
+                QueryDocumentSnapshot book = snapshot.data!.docs[index];
+                return buildCard(book);
+              },
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: _addButtonClicked, child: const Icon(Icons.add)),
     );
+  }
+
+  Card buildCard(QueryDocumentSnapshot book) {
+    var heading = book['name'];
+    var subheading = book['author'];
+    var cardImage = book['urlPicture'] == ""
+        ? const AssetImage('assets/images/logo.png') as ImageProvider
+        : NetworkImage(book['urlPicture']);
+    return Card(
+        elevation: 4.0,
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(heading,
+                  style: TextStyle(color: Colors.black, fontSize: 20)),
+              subtitle: Text(subheading,
+                  style: TextStyle(color: Colors.black54, fontSize: 18)),
+              /*   leading: Image.network(
+                book['urlPicture'] ?? "",
+                errorBuilder: (BuildContext context, Object exception,
+                    StackTrace? stackTrace) {
+                  return const Image(
+                    image: AssetImage('assets/images/logo.png'),
+                  );
+                },
+              ),*/
+            ),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              child: Ink.image(
+                image: cardImage,
+              ),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+          ],
+        ));
   }
 }
